e6b7d499c309b6fa53a083eeabaf28de00bed2b6
mannaKin <gkssk2286@naver.com>
2022-04-15 16:06:20 +0900

    Initial commit

--- src/days14/Array06.java
+++ src/days14/Array06.java
@@ -3,7 +3,7 @@ package days14;
 public class Array06 {
     public static void main(String[] args) {
         // 배열에 있는 값들 중 최댓값과 최솟값을 찾아서 출력
-        int [] a = {56,87,96,45,89,69,36,13,98,77};
+        int [] a = {-56,-87,-26,-45,-89,69,36,13,98,77};
         int max=a[0],min=a[0];
         for(int i=1;i<a.length;i++){
             if (max<a[i]) max=a[i];
