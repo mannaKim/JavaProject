5288e98ef70292d4a0240b7e1578bee23f8a5ce1
mannaKin <gkssk2286@naver.com>
2022-04-25 18:23:47 +0900

    Initial commit

---
+++ src/days21/Class08.java
@@ -0,0 +1,66 @@
+package days21;
+
+class Student {
+    int bunho;
+    String name;
+    int kor;
+    int eng;
+    int mat;
+    int tot;
+    double avg;
+
+    public int getBunho() {
+        return bunho;
+    }
+    public void setBunho(int bunho) {
+        this.bunho = bunho;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+    public void setKor(int kor) {
+        this.kor = kor;
+    }
+
+    public int getEng() {
+        return eng;
+    }
+    public void setEng(int eng) {
+        this.eng = eng;
+    }
+
+    public int getMat() {
+        return mat;
+    }
+    public void setMat(int mat) {
+        this.mat = mat;
+    }
+
+    public int getTot() {
+        return tot;
+    }
+    public void setTot(int tot) {
+        this.tot = tot;
+    }
+
+    public double getAvg() {
+        return avg;
+    }
+    public void setAvg(double avg) {
+        this.avg = avg;
+    }
+}
+
+public class Class08 {
+    public static void main(String[] args) {
+
+    }
+}
