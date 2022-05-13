f94e6f5debaf9d9167cab7b95ac1af27aa3c6754
mannaKin <gkssk2286@naver.com>
2022-05-13 15:44:11 +0900

    Initial commit

--- src/days33/Test0513.java
+++ src/days33/Test0513.java
@@ -1,7 +1,61 @@
 package days33;
 
-public class Test0513 {
-    public static void main(String[] args) {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+interface InterSuper1 {
+    public abstract void interSuper1Method();
+}
+interface InterSuper2 {
+    void interSuper2Method();
+}
+interface InterSub extends InterSuper1, InterSuper2 {
+        void interSubMethod();
+}
+class ClassSub1 implements InterSuper1, InterSuper2 {
+    @Override
+    public void interSuper1Method() {
+        System.out.println("interSuper1Method 메소드 실행");
+    }
+    @Override
+    public void interSuper2Method() {
+        System.out.println("interSuper2Method 메소드 실행");
     }
 }
+class ClassSub2 implements InterSub {
+    @Override
+    public void interSuper1Method() {
+        System.out.println("interSuper1Method 메소드 실행");
+    }
+    @Override
+    public void interSuper2Method() {
+        System.out.println("interSuper2Method 메소드 실행");
+    }
+    @Override
+    public void interSubMethod() {
+        System.out.println("interSubMethod 메소드 실행");
+    }
+}
+
+public class Test0513 {
+    public static void main(String[] args) {
+        ClassSub1 cs1 = new ClassSub1();
+        ClassSub2 cs2 = new ClassSub2();
+        test1(cs1);
+        //test1(cs2);
+        test2(cs1);
+        test2(cs2);
+
+        Date today = new Date();
+        System.out.println(today);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH:mm");
+        String file_name = sdf.format(today)+".dat";
+        System.out.println(file_name);
+
+        }
+    private static void test1(ClassSub1 cs1) {
+    }
+    private static void test2(InterSuper1 cs2) {
+    }
+}
+
